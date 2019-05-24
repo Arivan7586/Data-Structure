2ce9a90f9175c8faad9c0d3384f63bbfb3460818
Arivan7586 <1194028288@qq.com>
2019-05-24 17:21:39 +0800

    基于数组实现队列、基于链表实现队列

---
+++ src/arivan/Test5_24/MyQueue.java
@@ -0,0 +1,38 @@
+package arivan.Test5_23;
+
+/**
+ * 队列接口
+ * @param <E>
+ */
+public interface MyQueue<E> {
+
+    /**
+     * 向队列中加入数据
+     * @param e 需要插入的数据
+     */
+    void enQueue(E e);
+
+    /**
+     * 删除队列中的元素
+     * @return 返回被删除的元素
+     */
+    E deQueue();
+
+    /**
+     * 查看对头元素
+     * @return 返回对头元素
+     */
+    E peek();
+
+    /**
+     * 取得队列的长度
+     * @return 返回队列的元素个数
+     */
+    int getSize();
+
+    /**
+     * 判断队列是否为空
+     * @return 为空返回true，否则返回false
+     */
+    boolean isEmpty();
+}
