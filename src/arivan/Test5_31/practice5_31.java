4a4d6912d8fc4c4ec38704db1f3618d83e3fac61
Arivan7586 <1194028288@qq.com>
2019-05-31 15:00:19 +0800

    前序、中序、后序遍历二叉树，根据二叉树创建字符串（LeetCode）

--- src/arivan/Test5_31/practice5_31.java
+++ src/arivan/Test5_31/practice5_31.java
@@ -8,7 +8,8 @@ public class practice5_31 {
 
     public static void main(String[] args) {
 
-
+        Solution solution = new Solution();
+        Solution1 solution1 = new Solution1();
     }
 }
 
