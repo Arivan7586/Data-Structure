00faa093762d435f32fb1fa59aa1eba34003ffba
Arivan7586 <1194028288@qq.com>
2019-05-31 14:33:26 +0800

    二叉树的前序、中序、后序遍历（递归实现）。根据二叉树创建字符串

--- src/arivan/Test5_31/practice5_31.java
+++ src/arivan/Test5_31/practice5_31.java
@@ -5,9 +5,7 @@ import java.util.List;
 import java.util.Stack;
 
 public class practice5_31 {
-
     public static void main(String[] args) {
-
         Solution solution = new Solution();
     }
 }
